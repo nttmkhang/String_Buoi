30ada9df0196db8dfdfba7df5ef13479acd3a9c8
elodisme1 <22520254@gm.uit.edu.vn>
2023-07-17 09:18:48 +0700

    Do

--- VL20/VL20.cpp
+++ VL20/VL20.cpp
@@ -1,20 +1,24 @@
-// VL20.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+string InHoa(char, char);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char a, b;
+	cin >> a >> b;
+
+	cout << InHoa(a, b);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+string InHoa(char a, char b)
+{
+	stringstream stream;
+	for (int i = (int)a; i <= (int)b; i++)
+		stream << setw(6) << (char)toupper(i);
+	return stream.str();
+}
