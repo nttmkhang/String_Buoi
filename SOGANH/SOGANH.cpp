bd58d20974427369c62ec63b93c313f3489246c0
nghgi <22520357@gm.uit.edu.vn>
2023-07-27 21:40:16 +0700

    Giang

--- SOGANH/SOGANH.cpp
+++ SOGANH/SOGANH.cpp
@@ -1,34 +1,34 @@
-﻿#include <iostream>
-#include <string>
-#include <sstream>
-
-using namespace std;
-
-int SoGanh(string);
-string Process(string);
-
-int main()
-{
-	string a;
-	cin >> a;
-	cout << Process(a);
-	return 0;
-}
-
-int SoGanh(string a)
-{
-	int n = a.size();
-	int dem = 0;
-	string temp = "";
-		
-	if (dem != 0)
-		return dem;
-	return -1;
-}
-
-string Process(string a)
-{
-	stringstream stream;
-	stream << SoGanh(a);
-	return stream.str();
-}
+﻿//#include <iostream>
+//#include <string>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int SoGanh(string);
+//string Process(string);
+//
+//int main()
+//{
+//	string a;
+//	cin >> a;
+//	cout << Process(a);
+//	return 0;
+//}
+//
+//int SoGanh(string a)
+//{
+//	int n = a.size();
+//	int dem = 0;
+//	string temp = "";
+//		
+//	if (dem != 0)
+//		return dem;
+//	return -1;
+//}
+//
+//string Process(string a)
+//{
+//	stringstream stream;
+//	stream << SoGanh(a);
+//	return stream.str();
+//}
